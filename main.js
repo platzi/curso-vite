d30cc9e8349b07579eced570e1a4327b4d2e461f
Diana Martinez <diananerdoficial@gmail.com>
2022-04-26 18:22:37 -0500

    CSS Modules

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 import './style.css'
+import buttonSyles from './button.module.css'
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Platzi!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <button id="btn">Click!</button>
 `
+document.getElementById('btn').className = buttonSyles.btn
+
