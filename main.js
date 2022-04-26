f28390cce1c6e7cab0d5c4f285f47a05d4bb1b31
Diana Martinez <diananerdoficial@gmail.com>
2022-04-26 18:52:22 -0500

    Importar JSON

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ import './style.css'
 import buttonSyles from './button.module.css'
 import imageStyles from './image.module.css'
 import img from './vite-logo.png'
+import { user } from './data.json'
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Platzi!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
   <button id="btn">Click!</button>
   <img id="img" />
+  <pre>${JSON.stringify(user)}</pre>
 `
 document.getElementById('btn').className = buttonSyles.btn
 
