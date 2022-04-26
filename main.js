fb0bfdb84fc2fb4f962bfccea7b8d9cfa4a8acfb
Diana Martinez <diananerdoficial@gmail.com>
2022-04-26 18:38:17 -0500

    Importar archivos

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 import './style.css'
 import buttonSyles from './button.module.css'
+import imageStyles from './image.module.css'
+import img from './vite-logo.png'
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Platzi!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
   <button id="btn">Click!</button>
+  <img id="img" />
 `
 document.getElementById('btn').className = buttonSyles.btn
+
+const imagen = document.getElementById('img')
+imagen.src = img
+imagen.className = imageStyles.img
 
