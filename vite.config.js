bb36cf4bca1801f03e4c4618aa6ce8b3b10789ea
Diana Martinez <diananerdoficial@gmail.com>
2022-04-27 17:37:16 -0500

    Multi paginas

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig(({ command, mode }) => {
   const port = 3000
@@ -10,13 +11,22 @@ export default defineConfig(({ command, mode }) => {
 
   if (mode === "development") {
     console.log("modo desarrollo")
+    return {
+      server: {
+        port
+      }
+    }
   } else {
     console.log("modo produccion")
-  }
-
-  return {
-    server: {
-      port
+    return {
+      build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, 'index.html'),
+            help: resolve(__dirname, 'help', 'help.html')
+          }
+        }
+      }
     }
   }
 })
