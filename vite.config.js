fc3b309734dfadfba255206d17415bbd77e68a64
Diana Martinez <diananerdoficial@gmail.com>
2022-04-27 16:46:30 -0500

    Variables de entorno y modos

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig(() => {
+export default defineConfig(({ command, mode }) => {
   const port = 3000
+
+  console.log(command, mode)
+  const env = loadEnv(mode, process.cwd())
+
+  console.log(env.VITE_NAME)
+
+  if (mode === "development") {
+    console.log("modo desarrollo")
+  } else {
+    console.log("modo produccion")
+  }
 
   return {
     server: {
